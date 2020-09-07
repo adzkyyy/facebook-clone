9d05f86a285cd71dabddbca0257b8968412d9962
adzkyyy <adzkizaki24@gmail.com>
2020-09-08 00:02:30 +0700

    deploy

--- src/komponen/Widget.js
+++ src/komponen/Widget.js
@@ -5,7 +5,7 @@ function Widget() {
   return (
     <div className="widget">
       <iframe
-        src="https://www.facebook.com/plugins/page.php?href=http%3A%2F%2Fwww.facebook.com%2FCleverProgrammerr%2F&tabs=timeline&width=340&height=2500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+        src="http://netflix-clone-react.surge.sh/"
         width="340"
         height="100%"
         style={{ border: "none", overflow: "hidden" }}
